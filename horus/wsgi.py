7f004fc749b4ff49686a76e44f54975a2600d34a
Don Shields <zilla@Dons-iMac.local>
2016-07-18 16:02:25 -0700

    Modified to work w/ apache

--- horus/wsgi.py
+++ horus/wsgi.py
@@ -7,10 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
-
+sys.path.append('/var/www/projectHorus/')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "horus.settings")
 
 application = get_wsgi_application()
